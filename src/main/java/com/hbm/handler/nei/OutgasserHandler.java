cf9d5453a5cfb7c99d28e3002281824eff4f1b22
Smug-Bab <87583840+Smug-Bab@users.noreply.github.com>
2026-05-02 20:50:05 +0100

    .

--- src/main/java/com/hbm/handler/nei/OutgasserHandler.java
+++ src/main/java/com/hbm/handler/nei/OutgasserHandler.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.hbm.handler.nei;
 
 import java.awt.Rectangle;
@@ -27,3 +28,34 @@ public class OutgasserHandler extends NEIUniversalHandler {
 		RecipeTransferRectHandler.registerRectsToGuis(guiGui, transferRectsGui);
 	}
 }
+=======
+package com.hbm.handler.nei;
+
+import java.awt.Rectangle;
+
+import com.hbm.blocks.ModBlocks;
+import com.hbm.inventory.gui.GUIRBMKOutgasser;
+import com.hbm.inventory.recipes.OutgasserRecipes;
+
+import net.minecraft.item.ItemStack;
+
+public class OutgasserHandler extends NEIUniversalHandler {
+
+	public OutgasserHandler() {
+		super(ModBlocks.rbmk_outgasser.getLocalizedName(), new ItemStack[] {new ItemStack(ModBlocks.rbmk_outgasser), new ItemStack(ModBlocks.fusion_breeder)}, OutgasserRecipes.getRecipes());
+	}
+
+	@Override
+	public String getKey() {
+		return "ntmOutgasser";
+	}
+	
+	@Override
+	public void loadTransferRects() {
+		super.loadTransferRects();
+		transferRectsGui.add(new RecipeTransferRect(new Rectangle(75, 26, 16, 32), "ntmOutgasser"));
+		guiGui.add(GUIRBMKOutgasser.class);
+		RecipeTransferRectHandler.registerRectsToGuis(guiGui, transferRectsGui);
+	}
+}
+>>>>>>> parent of ab86f3f (update cuz I felt like it)
